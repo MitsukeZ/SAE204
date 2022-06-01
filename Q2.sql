a89d3fcabd5271bbdd99f22133f8f9c4d2fc898e
mitsu <florianbizet2@gmail.com>
2022-06-01 15:06:36 +0200

    Query 2 + removal of pdf

---
+++ Q2.sql
@@ -0,0 +1,6 @@
+/*
+b) Lister les clients (numéro, nom) et leur nombre d’achats (que l’on nommera nbA) 
+triés par ordre décroissant de leur nombre d’achats (sans prendre en compte la quantité achetée)
+*/
+
+SELECT numClient, nomClient
