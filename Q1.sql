30d7fb1f4d5374cff7a203564e86432f9edcdfc4
mitsu <florianbizet2@gmail.com>
2022-06-01 15:02:05 +0200

    Query 1

--- Q1.sql
+++ Q1.sql
@@ -5,3 +5,20 @@ a) Lister les clients (numéro et nom) triés par ordre alphabétique du nom
 SELECT   numClient, nomClient
 FROM     Client
 ORDER BY nomClient;
+
+/*
+ numclient |  nomclient  
+-----------+-------------
+         7 | Don Devello
+         2 | Fissile
+         9 | Ginal
+         3 | Hauraque
+        10 | Hautine
+        11 | Kament
+         5 | Menvussa
+         8 | Ohm
+         4 | Poret
+         6 | Timable
+         1 | Torguesse
+(11 rows)
+*/
