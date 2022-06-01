3dd550c78c59693043011bd233af470dd8b44946
mitsu <florianbizet2@gmail.com>
2022-06-01 15:14:08 +0200

    Query 2 finished

--- Q2.sql
+++ Q2.sql
@@ -3,4 +3,25 @@ b) Lister les clients (numéro, nom) et leur nombre d’achats (que l’on nomme
 triés par ordre décroissant de leur nombre d’achats (sans prendre en compte la quantité achetée)
 */
 
-SELECT numClient, nomClient
+SELECT c.numClient, c.nomClient, SUM(co.quantite) AS nbA
+FROM   Client c JOIN Vente v      ON c.numClient = v.numClient
+                JOIN Concerner co ON v.numVente = co.numVente
+GROUP BY c.numClient
+ORDER BY nbA DESC;
+
+/*
+ numclient |  nomclient  | nba  
+-----------+-------------+------
+         4 | Poret       | 8816
+         6 | Timable     | 6639
+         8 | Ohm         | 6159
+         7 | Don Devello | 5664
+         9 | Ginal       | 4564
+         3 | Hauraque    | 4307
+         5 | Menvussa    | 4093
+        10 | Hautine     | 3865
+         2 | Fissile     | 3651
+         1 | Torguesse   | 2645
+        11 | Kament      | 1691
+(11 rows)
+*/
