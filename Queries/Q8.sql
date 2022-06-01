2be39e66f23c5d0edbe2f3c11a694759ad942e56
Floris <Floris.robart11@gmail.com>
2022-06-01 16:31:54 +0200

    tentative1 de Q8

--- Queries/Q8.sql
+++ Queries/Q8.sql
@@ -5,5 +5,16 @@ mail, ainsi que le nombre total de BD qu’ils ont acheté (champ nbBD en tenant
 compte des quantités achetées), ainsi que le total de leurs achats (champ coutA).
 */
 
-CREATE VIEW AS
-Select numClient, nomClient, mailClient, 
+DROP VIEW IF EXISTS best5;
+
+CREATE VIEW best5 AS
+SELECT numClient, nomClient, mailClient, SUM(quantite) AS nbBD
+
+FROM   Client cl JOIN Vente v
+       ON cl.numClient = v.numClient
+       JOIN Concerner co
+       ON v.numVente = co.numVente
+
+ORDER BY SUM(quantite * prixVente)
+
+LIMIT 5;
