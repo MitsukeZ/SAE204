3d1d9bdea419ea1fc78f46165271b02a08e5185c
mitsu <florianbizet2@gmail.com>
2022-06-01 16:15:00 +0200

    Requete 9

---
+++ Queries/Q7.sql
@@ -0,0 +1,24 @@
+/*
+g) Lister les clients (numéro et nom) qui n’ont acheté que les BD de la série ‘Asterix
+le gaulois’ (en utilisant la clause EXCEPT)
+*/
+
+SELECT DISTINCT c.numClient, c.nomClient
+FROM            Client c JOIN Vente v      ON c.numClient = v.numClient
+                         JOIN Concerner co ON v.numVente  = co.numVente
+                         JOIN BD           ON co.isbn     = BD.isbn
+                         JOIN Serie s      ON BD.numSerie = s.numSerie
+EXCEPT
+SELECT DISTINCT c.numClient, c.nomClient
+FROM            Client c JOIN Vente v      ON c.numClient = v.numClient
+                         JOIN Concerner co ON v.numVente  = co.numVente
+                         JOIN BD           ON co.isbn     = BD.isbn
+                         JOIN Serie s      ON BD.numSerie = s.numSerie
+WHERE s.nomSerie != 'Asterix le gaulois'
+
+/*
+ numclient | nomclient 
+-----------+-----------
+         3 | Hauraque
+(1 row)
+*/
