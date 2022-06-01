c2874701d8629b69abe4838729b212bd3255c57c
mitsu <florianbizet2@gmail.com>
2022-06-01 15:57:17 +0200

    Requete 5

---
+++ Queries/Q5.sql
@@ -0,0 +1,46 @@
+/*
+e) Créer une vue appelée CA qui affiche le chiffre d’affaire réalisé 
+par année en listant dans l’ordre croissant des années 
+(champ appelé annee) et en face le chiffre réalisé (appelé chA).
+*/
+
+DROP VIEW IF EXISTS CA;
+
+CREATE VIEW CA AS 
+SELECT   EXTRACT(YEAR FROM v.dteVente) AS annee, SUM(co.prixVente * co.quantite) as chiffreAffaires
+FROM     Vente v JOIN Concerner co ON v.numVente = co.numVente
+GROUP BY annee
+ORDER BY annee;
+
+--Requete pour voir le contenu de la vue
+
+SELECT * FROM CA;
+
+/*
+ annee |  chiffreaffaires   
+-------+--------------------
+  2000 |              34059
+  2001 |           52773.45
+  2002 |              46129
+  2003 |            15867.5
+  2004 |            45393.9
+  2005 |            64904.7
+  2006 |            14602.5
+  2007 |            34254.8
+  2008 |            19389.8
+  2009 |            14755.2
+  2010 | 15545.400000000001
+  2011 |            17137.5
+  2012 |            29922.4
+  2013 |              11316
+  2014 |            46403.7
+  2015 |             9294.3
+  2016 |            25570.3
+  2017 |            23346.3
+  2018 |            76295.8
+  2019 |              59304
+  2020 |              24000
+  2021 |            54833.6
+(22 rows)
+*/
+
