f61e554968b4a0ef9748c18030f225df1f60394e
Floris <Floris.robart11@gmail.com>
2022-06-08 10:54:10 +0200

    Q1 finito

--- Functions/Q1.sql
+++ Functions/Q1.sql
@@ -4,18 +4,20 @@ qui renvoie pour chaque titre de BD de l’auteur, le nombre d’exemplaires ven
 de cette BD.
 */
 
-CREATE OR REPLACE FUNCTION getVentesParAuteur(idauteur Auteur.numAuteur%TYPE)
-RETURNS Concerner.quantite%TYPE AS
+CREATE OR REPLACE FUNCTION getVentesParAuteur(numDessinateur Auteur.numAuteur%TYPE)
+RETURNS SETOF Concerner.quantite%TYPE AS
 $$
 
 DECLARE
-    quantiteTotal Concerner.quantite%TYPE;
-
+    qte Concerner.quantite%TYPE;
 
 BEGIN
-    FOR 
-
-
-
+    FOR qte IN SELECT   sum(quantite)
+                 FROM     Concerner c JOIN Bd ON c.isbn = Bd.isbn
+                 WHERE    numAuteurDessinateur = numDessinateur
+                 GROUP BY Bd.isbn
+    LOOP
+        return NEXT qte;
+    END LOOP;
 END
 $$ LANGUAGE plpgsql;
