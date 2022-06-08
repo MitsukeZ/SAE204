efdf6d1be9d56e559ab4a5aef5f14313bf6d9fa7
Floris <Floris.robart11@gmail.com>
2022-06-08 16:48:59 +0200

    Q1 visualisation + 1,2,3 privilège

---
+++ Privilege/Q2.sql
@@ -0,0 +1,7 @@
+/*
+b) le vendeur a les droits en lecture/écriture sur la table des ventes et des clients et
+sur la table Concerner entre les deux.
+*/
+grant SELECT,INSERT
+   ON Vente, Client, Concerner
+   to vendeur;
---
+++ Visualisation/Q1.sql
@@ -0,0 +1,12 @@
+/*
+a) représenter dans le tableur de Libre Office par une courbe des valeurs
+d’évolution des ventes de BD (chiffre d’affaires) par année de vente (dans l’ordre
+croissant des années). Les années sont en abscisse et le chiffre d’affaire en
+ordonnée.
+*/
+
+SELECT EXTRACT(YEAR FROM dteVente), sum(quantite*prixVente) AS Chiffre_affaire FROM   Concerner NATURAL JOIN Vente GROUP BY EXTRACT(YEAR FROM dteVente) ORDER BY EXTRACT(YEAR FROM dteVente) ASC;
+
+
+
+\copy (SELECT EXTRACT(YEAR FROM dteVente), sum(quantite*prixVente) AS Chiffre_affaire FROM   Concerner NATURAL JOIN Vente GROUP BY EXTRACT(YEAR FROM dteVente) ORDER BY EXTRACT(YEAR FROM dteVente) ASC;) TO './Question_A.csv' DELIMITER ';' QUOTE '"' CSV HEADER
